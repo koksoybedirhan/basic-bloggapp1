27633bffe9066c66b29b0c8b5329d01060186dee
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-03-07 11:29:41 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -16,12 +16,64 @@
     <br>
     <div class="row">
         <div class="col-12">
-    
-           <h3>Merhaba: <?php echo htmlspecialchars($_SESSION["username"])?></h3>
+            <section style="background-color: #f4f5f7;">
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <div class="card mb-3" style="border-radius: .5rem;">
+                        <div class="row g-0">
+                            <div class="col-md-4 gradient-custom text-center text-white"
+                            style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+                            <img src="img/<?=$_SESSION['image']?>"
+                                alt="Avatar" class="img-fluid my-5" width="400"/>
+                            <h5><?php echo htmlspecialchars($_SESSION["username"])?></h5>
+                            <i class="far fa-edit mb-5"></i>
+                            </div>
+                            <div class="col-md-8">
+                            <div class="card-body p-4">
+                                <h6>Information</h6>
+                                <hr class="mt-0 mb-4">
+                                <div class="row pt-1">
+                                <div class="col-4 mb-3">
+                                    <h6>Mail Adresi</h6>
+                                    <p class="text-muted"><?php echo htmlspecialchars($_SESSION["mail"])?></p>
+                                </div>
+                                <div class="col-4 mb-3">
+                                    <h6>Numara</h6>
+                                    <p class="text-muted"><?php echo htmlspecialchars($_SESSION["number"])?></p>
+                                </div>
+                                <div class="col-4 mb-3">
+                                    <h6>Kullanıcı Yetkisi</h6>
+                                    <p class="text-muted"><?php echo htmlspecialchars($_SESSION["userType"])?></p>
+                                </div>
+                                </div>
+                                <h6>Projects</h6>
+                                <hr class="mt-0 mb-4">
+                                <div class="row pt-1">
+                                <div class="col-6 mb-3">
+                                    <h6>Recent</h6>
+                                    <p class="text-muted">Lorem ipsum</p>
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <h6>Most Viewed</h6>
+                                    <p class="text-muted">Dolor sit amet</p>
+                                </div>
+                                </div>
+                                <div class="d-flex justify-content-start">
+                                <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
+                                <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
+                                <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br>
            <div>
                 <a href="logout.php" class="btn btn-danger">Logout</a>
            </div>
-
         </div>    
     </div>
     <br>
