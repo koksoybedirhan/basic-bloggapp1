48ea37d36d636b423521b4d4111071a679050f26
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-03-12 11:52:37 +0300

    Update footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -3,16 +3,19 @@
         <div class="container p-4 pb-0">
         <section class="">
             <p class="d-flex justify-content-center align-items-center">
-            <span class="me-3">Üyelik için tıklayınız.</span>
-            <a href="./register.php"><button type="button" class="btn btn-outline-light btn-rounded">
-                Üye Olma
-            </button></a>
+                <?php if (isLoggedin()): ?>
+                    <span class="me-3">Çıkış için tıklayınız.</span>
+                    <a href="./logout.php"><button type="button" class="btn btn-outline-light btn-rounded">Çıkış</button></a>
+                <?php else: ?>
+                    <span class="me-3">Üyelik için tıklayınız.</span>
+                    <a href="./register.php"><button type="button" class="btn btn-outline-light btn-rounded">Üye Olma</button></a>
+                <?php endif;?>
             </p>
         </section>
         </div>
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2023 Copyright:
-        <a class="text-white" href="#">bedirhankoksoy.com</a>
+        <a class="text-white" href="about.php">bedirhankoksoy.com</a>
         </div>
     </footer>
 </section>
