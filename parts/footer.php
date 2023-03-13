3cf26bdba9ef0e7eec76eac1778af41dd07925d6
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-03-13 13:04:28 +0300

    Update footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -8,7 +8,7 @@
                     <a href="./logout.php"><button type="button" class="btn btn-outline-light btn-rounded">Çıkış</button></a>
                 <?php else: ?>
                     <span class="me-3" >Giriş için tıklayınız.</span>
-                    <a href="./register.php" style="margin-right: 15px;"><button type="button" class="btn btn-outline-light btn-rounded" >Giriş</button></a>
+                    <a href="./login.php" style="margin-right: 15px;"><button type="button" class="btn btn-outline-light btn-rounded" >Giriş</button></a>
                     <span class="me-3" style="margin-left: 15px;">Üyelik için tıklayınız.</span>
                     <a href="./register.php"><button type="button" class="btn btn-outline-light btn-rounded">Üye Olma</button></a>
                 <?php endif;?>
