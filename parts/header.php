93b6b20eb51216c7673ec67b95c42c79c7a49ffc
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-03-13 16:23:54 +0300

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -8,5 +8,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Index</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title> 
 </head>
