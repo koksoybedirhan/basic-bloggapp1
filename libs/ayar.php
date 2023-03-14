df2d10c08d01e68d99466ce973211076684a6273
Bedirhan Köksoy <71734713+koksoybedirhan@users.noreply.github.com>
2023-03-15 00:49:33 +0300

    Update ayar.php

--- libs/ayar.php
+++ libs/ayar.php
@@ -3,7 +3,7 @@
     $server ="localhost";
     $username = "root";
     $password = "";
-    $database = "genel2";
+    $database = "genel1";
 
     $baglanti = mysqli_connect($server, $username, $password, $database);
     mysqli_set_charset($baglanti, "UTF8");
